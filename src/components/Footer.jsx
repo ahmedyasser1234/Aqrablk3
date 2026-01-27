7587a7cb18ff438897e62ec4634e5b4456fa6b38
ahmedyasser1234 <44351191+ahmedyasser1234@users.noreply.github.com>
2026-01-27 19:53:59 +0200

    Add contact information to the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,6 +35,12 @@ const Footer = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
               </svg>
             </div>
+              <div className="flex items-center gap-2 md:gap-3 group">
+              <span className="text-white text-base md:text-xl font-bold order-1" dir="ltr">01014700317</span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 md:w-5 md:h-5 text-blue-400 opacity-70 group-hover:opacity-100 transition-opacity order-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+              </svg>
+            </div>
             <div className="flex items-center gap-2 md:gap-3 group mt-0.5">
               <a href="mailto:info@aqrablkmedia.com" className="hover:text-blue-400 transition-colors text-xs md:text-lg lowercase order-1">info@aqrablkmedia.com</a>
               <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 md:w-5 md:h-5 text-blue-400 opacity-70 group-hover:opacity-100 transition-opacity order-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
