4aef224ae7a87ecc13d45fe3ef9eb24c66a3c1d2
ahmedyasser1234 <44351191+ahmedyasser1234@users.noreply.github.com>
2026-01-27 19:51:25 +0200

    Change audio source for glass break sound

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
 
   const triggerCrack = () => {
     try {
-      const audio = new Audio('/audio/glass-break.mp3');
+      const audio = new Audio('/audio/glass-break2.mp3');
       audio.volume = 0.4;
       audio.currentTime = 0;
       audio.play().catch(() => { });
