37a0c13e276bb14521ab0509ec1eabf42a0fd1dd
ahmedyasser1234 <44351191+ahmedyasser1234@users.noreply.github.com>
2026-01-27 19:43:40 +0200

    Update API and SOCKET URLs for production

--- src/config.js
+++ src/config.js
@@ -1,10 +1,3 @@
 // src/config.js
-const isProduction = window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1';
-
-export const API_BASE_URL = isProduction
-    ? `${window.location.origin}/api` // Usually standard for Nginx proxy pass
-    : 'http://127.0.0.1:3545';
-
-export const SOCKET_URL = isProduction
-    ? window.location.origin
-    : 'http://127.0.0.1:3545';
+export const API_BASE_URL = 'https://aqrablkmedia.com/api';
+export const SOCKET_URL = 'https://aqrablkmedia.com';
